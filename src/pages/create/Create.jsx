004a61fd5f8c0805e261f20f62aa9e99c34e05d1
Megagig <megagigsoftwaresolutions.com>
2024-05-10 17:20:35 +0100

    Setup Project Folder Structure

---
+++ src/pages/create/Create.jsx
@@ -0,0 +1,7 @@
+import './Create.css';
+
+const Create = () => {
+  return <div>Create recipe page</div>;
+};
+
+export default Create;
---
+++ src/pages/recipe/Recipe.jsx
@@ -0,0 +1,12 @@
+import './Recipe.css';
+
+const Recipe = () => {
+  return (
+    <div>
+      <h1>Recipe Page</h1>
+      <p>Recipe details</p>
+    </div>
+  );
+};
+
+export default Recipe;
