ff5670b7c34aa29e4dfd2ba8bd9e4c3d6742a5ed
Megagig <megagigsoftwaresolutions.com>
2024-05-12 13:10:33 +0100

    Fetching a single Recipe

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,10 +1,29 @@
+import { useParams } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
+
 import './Recipe.css';
 
 const Recipe = () => {
+  const { id } = useParams();
+  const url = `http://localhost:3000/recipes/${id}`;
+  const { data: recipe, isLoading, error } = useFetch(url);
+
   return (
-    <div>
-      <h1>Recipe Page</h1>
-      <p>Recipe details</p>
+    <div className="recipe">
+      {error && <p className="error">{error}</p>}
+      {isLoading && <p className="loading">Loading...</p>}
+      {recipe && (
+        <div>
+          <h2>{recipe.title}</h2>
+          <p>{recipe.cookingTime}</p>
+          <ul>
+            {recipe.ingredients.map((ingredient, recipe) => (
+              <li key={recipe}>{ingredient}</li>
+            ))}
+          </ul>
+          <p>{recipe.method}</p>
+        </div>
+      )}
     </div>
   );
 };
