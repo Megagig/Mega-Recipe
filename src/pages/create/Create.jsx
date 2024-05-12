26a970c1f0215bc1192513d37a930f7668cb622e
Megagig <megagigsoftwaresolutions.com>
2024-05-12 17:17:50 +0100

    Adding multiple ingredients

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,18 +1,31 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './Create.css';
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [method, setMethod] = useState('');
   const [cookingTime, setCookingTime] = useState('');
+  const [newIngredient, setNewIngredient] = useState('');
+  const [ingredients, setIngredients] = useState([]);
+  const ingredientInput = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const newRecipe = { title, method, cookingTime };
+    const newRecipe = { title, method, cookingTime, ingredients };
     console.log(newRecipe);
     setTitle('');
     setMethod('');
     setCookingTime(''); // clear the form after submission
+  };
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const ingredient = newIngredient.trim();
+    if (ingredient && !ingredients.includes(ingredient)) {
+      setIngredients((prevIngredients) => [...prevIngredients, ingredient]);
+    }
+    setNewIngredient('');
+    ingredientInput.current.focus();
   };
   return (
     <div className="create">
@@ -26,6 +39,28 @@ const Create = () => {
           onChange={(e) => setTitle(e.target.value)}
           required
         ></input>
+        {/* Ingredients go here */}
+        <label htmlFor="ingredients">Recipe Ingredients</label>
+        <div className="ingredients">
+          <input
+            type="text"
+            id="ingredients"
+            placeholder="Ingredients"
+            onChange={(e) => setNewIngredient(e.target.value)}
+            value={newIngredient}
+            ref={ingredientInput}
+          ></input>
+          <button onClick={handleAdd} className="btn">
+            Add
+          </button>
+        </div>
+        <p>
+          current Ingredients:{' '}
+          {ingredients.map((ingredient, index) => (
+            <em key={index}>{ingredient}, </em>
+          ))}
+        </p>
+
         <label htmlFor="method">Recipe Method</label>
         <textarea
           id="method"
